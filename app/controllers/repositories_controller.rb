3e3376ddd6153f9031e126d6cbc3e68490963122
kellyeryan <kellyeryan@gmail.com>
2020-01-27 16:32:45 -0500

    Finish rails github api lab

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,9 @@
 class RepositoriesController < ApplicationController
-  
   def index
+    @resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.params["oauth_token"] = session[:token]
+    end
+    @body = JSON.parse(@resp.body)
   end
 
 end
